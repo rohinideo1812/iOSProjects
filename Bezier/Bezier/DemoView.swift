56fd5a1a2ed9461bb70c53c5aa6a152442ff410d
BridgeLabz <>
2018-07-11 17:41:31 +0530

    commit

---
+++ Bezier/Bezier/DemoView.swift
@@ -0,0 +1,66 @@
+import UIKit
+
+
+class DemoView: UIView {
+
+    var path: UIBezierPath!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = UIColor.gray
+//        let image = #imageLiteral(resourceName: "cover")
+//        let imageView = UIImageView(image: image)
+//        imageView.frame = CGRect(x: 0, y: 0, width: frame.size.width, height: frame.size.height)
+//        addSubview(imageView)
+    }
+    
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    
+    override func draw(_ rect: CGRect) {
+        self.createRectangle()
+        
+        UIColor.orange.setFill()
+        path.fill()
+        
+        // Specify a border (stroke) color.
+        UIColor.purple.setStroke()
+        path.stroke()
+        
+        let shapeLayer = CAShapeLayer()
+        shapeLayer.path = path.cgPath
+        self.layer.mask = shapeLayer
+        
+        
+        //new
+        let image = #imageLiteral(resourceName: "cover")
+        let imageView = UIImageView(image: image)
+        imageView.frame = CGRect(x: 0, y: 0, width: frame.size.width, height: frame.size.height)
+        addSubview(imageView)
+    }
+    
+    
+    func createRectangle() {
+        // Initialize the path.
+        path = UIBezierPath()
+        
+        // Specify the point that the path should start get drawn.
+        path.move(to: CGPoint(x: 0.0, y: 0.0))
+        
+        // Create a line between the starting point and the bottom-left side of the view.
+        path.addLine(to: CGPoint(x: 0.0, y: self.frame.size.height))
+        
+        // Create the bottom line (bottom-left to bottom-right).
+        path.addLine(to: CGPoint(x: self.frame.size.width, y: self.frame.size.height - 100))
+        
+        // Create the vertical line from the bottom-right to the top-right side.
+        path.addLine(to: CGPoint(x: self.frame.size.width, y: 0.0))
+        
+        // Close the path. This will create the last line automatically.
+        path.close()
+
+    }
+}
---
+++ Bezier/Bezier/ViewController.swift
@@ -0,0 +1,36 @@
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var signInBtn: UIButton!
+    @IBOutlet weak var signUpBtn: UIButton!
+    @IBOutlet weak var forgotPsswrdBtn: UIButton!
+    @IBOutlet weak var passwordTxtFeld: UITextField!
+    @IBOutlet weak var emailTxtField: UITextField!
+    
+    @IBOutlet weak var lbl: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.forgotPsswrdBtn.setTitleColor(UIColor(hexString: "#ff8080"), for: UIControlState.normal)
+        self.signUpBtn.setTitleColor(UIColor(hexString: "#ff8080"), for: UIControlState.normal)
+        self.signInBtn.setTitleColor(UIColor.white, for: UIControlState.normal)
+       self.signInBtn.backgroundColor = UIColor(hexString: "#ff8080")
+       self.lbl.textColor = UIColor.lightGray
+       
+
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.emailTxtField.useUnderline()
+        self.passwordTxtFeld.useUnderline()
+        self.emailTxtField.layer.borderColor = UIColor(hexString: "#ff8080").cgColor
+        self.passwordTxtFeld.layer.borderColor = UIColor(hexString: "#ff8080").cgColor
+
+    }
+    
+    
+    
+ }
+
